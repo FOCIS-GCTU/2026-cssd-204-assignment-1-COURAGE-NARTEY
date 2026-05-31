512c4d9bf5e5d677b65db12e2562056a2cf9f54a
COURAGE-NARTEY <2425401317@live.gctu.edu.gh>
2026-05-31 10:37:32 +0000

    Typed my initial code

--- CTN.py
+++ CTN.py
@@ -1,6 +1,18 @@
+# File: CTN.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Courage Teye Nartey
+# STUDENT ID: 2425401317
+# Email: 2425401317@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Courage Teye Nartey, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  def main():
+  
     # Print initials CTN in small letters then in large stylized block letters,
     # each letter 12 characters wide by 10 characters high, followed by
     # a 2x2 asterisk period block. Three period columns separate each element.
@@ -20,10 +32,6 @@ def main():
     print("...CC.............**........TT........**...N.......NNNN...**")
     print("....CCCCCCCCCC....**........TT........**...N........NNN...**")
     print()
-
-
-main()
-
 
 
 main()
